2a5e8a33db9fc3ed0abd7f4debacfa0dc2007aa2
MohammedHW <hoshsangabadwalam@gmail.com>
2026-08-27 12:31:56 +0530

    fix(driver-payout): restore GetX builder callback syntax

--- lib/page/add_bank_details/add_bank_account.dart
+++ lib/page/add_bank_details/add_bank_account.dart
@@ -23,6 +23,7 @@ class AddBankAccount extends StatelessWidget {
     final themeChange = Provider.of<DarkThemeProvider>(context);
     return GetX<BankDetailsController>(
         init: BankDetailsController(),
+        builder: (controller) {
           return Container(
             decoration: BoxDecoration(
               color: themeChange.getThem() ? AppThemeData.grey50Dark : AppThemeData.grey50,
@@ -306,6 +307,7 @@ class AddBankAccount extends StatelessWidget {
                     ),
                   )
                 ],
+              ),
             ),
           );
         });
