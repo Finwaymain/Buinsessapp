494354911e0907ee8d49ca956124da4cd1d657bd
MohammedHW <hoshsangabadwalam@gmail.com>
2026-09-25 16:37:50 +0530

    feat(finance): link loans to finance webview portal with driver context

--- lib/page/wallet/widgets/wallet_overview_tab.dart
+++ lib/page/wallet/widgets/wallet_overview_tab.dart
@@ -637,9 +637,10 @@ class WalletOverviewTab extends StatelessWidget {
         'name': fullName,
         'mobile': mobile,
         'pocket_number': pocketNumber,
+        'user_type': 'driver',
       };
 
-      final uri = Uri.parse("https://api.fiinway.com/loans/coming-soon")
+      final uri = Uri.parse("https://api.fiinway.com/finance")
           .replace(queryParameters: queryParams);
 
       Get.to(() => WebViewScreen(
